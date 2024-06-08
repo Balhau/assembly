c257888e395a95a41651fc11f397d4cc86f59c0f
Balhau <balhau@balhau.net>
2024-06-08 20:32:10 +0100

    More on calling conventions

--- x86abi.c
+++ x86abi.c
@@ -1,15 +1,20 @@
 /**
-*
-* In this c file we will explore the x86 convention calls.
-* We create a set of function definitions and we will implement those in
-* an assembly file called x86abi.s with the corresponding assembly code.
-* This file aims to explore and understand the convention calling system.
-*
-* By convention call system we mean how the c code is able to call assembly code. This means exploring the different ways registers are selected to be used as function argument parameters and return values as well. The calling convention specifies which registers will be used for the arguments and for the return values for each of the different function types. 
-* This is, obviously specific per architecture since different architectures have a different instruction set. It is also dependent on the compiler may use different calling conventions. 
-* SYSTEM V amd abi
-* The first six integer or pointer arguments are passed in registers: %rdi, %rsi, %rdx, %rcx, %r8, %r9.
-*/
+ *
+ * In this c file we will explore the x86 convention calls.
+ * We create a set of function definitions and we will implement those in
+ * an assembly file called x86abi.s with the corresponding assembly code.
+ * This file aims to explore and understand the convention calling system.
+ *
+ * By convention call system we mean how the c code is able to call assembly
+ * code. This means exploring the different ways registers are selected to be
+ * used as function argument parameters and return values as well. The calling
+ * convention specifies which registers will be used for the arguments and for
+ * the return values for each of the different function types. This is,
+ * obviously specific per architecture since different architectures have a
+ * different instruction set. It is also dependent on the compiler may use
+ * different calling conventions. SYSTEM V amd abi The first six integer or
+ * pointer arguments are passed in registers: %rdi, %rsi, %rdx, %rcx, %r8, %r9.
+ */
 
 #include <stdio.h>
 
@@ -19,12 +24,37 @@ typedef struct MyStruct {
   char aChar;
 } MyStruct;
 
+void printListaInt(int lista[], int length) {
+  for (int i = 0; i < length; i++) {
+    printf("%d, ", lista[i]);
+  }
+}
+
+void printListaDouble(double lista[], int length) {
+  for (int i = 0; i < length; i++) {
+    printf("%f, ", lista[i]);
+  }
+}
+
 void doStuff(void);
 void doubleMyInt(int);
-void doubleMyIntArray(int[]);
+void doubleMyIntArray(int[], int);
+double addDoubles(double[], int);
 
-int main(void){
+int main(void) {
+  int list_int[] = {1, 2, 3, 4};
+  double list_double[] = {1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0, 10.0};
+  double sum = 0;
   printf("Hello world\n");
   doStuff();
   doubleMyInt(2);
+  printf("\nBefore:\n");
+  printListaInt(list_int, 4);
+  doubleMyIntArray(list_int, 4);
+  printf("\nAfter:\n");
+  printListaInt(list_int, 4);
+  printf("\nLista Doubles:\n");
+  printListaDouble(list_double, 10);
+  sum = addDoubles(list_double, 10);
+  printf("\nSum of doubles: %f\n", sum);
 }
--- x86abi.s
+++ x86abi.s
@@ -1,6 +1,7 @@
   .global doStuff
   .global doubleMyInt
   .global doubleMyIntArray
+  .global addDoubles
   .text
 
 doStuff:
@@ -29,7 +30,46 @@ doubleMyInt:
   ret
 
 doubleMyIntArray:
+  push %rbp
+  mov %rsp, %rbp
+  
+  xor %rdx, %rdx
+  cmp %rdx, %rsi
+  je .loop_end_1
+
+.loop_1:
+  mov (%rdi,%rdx, 4), %ecx      # use 32 bit register instead of 8bytes because its int 32 bit num not 64 (rdi, rdx, 4) = rdi+(rdx*4)
+  add %ecx, %ecx
+  mov %ecx, (%rdi, %rdx, 4)
+  inc %rdx
+  cmp %rdx, %rsi
+  jne .loop_1
+
+.loop_end_1:
+  pop %rbp
   ret
+
+
+addDoubles:
+  push %rbp
+  mov %rsp, %rbp
+  
+  xor %rdx, %rdx
+  cvtsi2sd %rdx, %xmm1    # this is mmx the assembly instruction that enable conversion of values stored in 64 bit registers into 128 bit xmm1 ones
+  cmp %rdx, %rsi
+  je .loop_end_2
+
+.loop_2:
+  movsd (%rdi, %rdx, 8), %xmm0
+  addsd %xmm0, %xmm1
+  inc %rdx
+  cmp %rdx, %rsi
+  jne .loop_2
+
+.loop_end_2:
+  movsd %xmm1, %xmm0
+  pop %rbp;
+  ret;
 
 .data
 msg:
